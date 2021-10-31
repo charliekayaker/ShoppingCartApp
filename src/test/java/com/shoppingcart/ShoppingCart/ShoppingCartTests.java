45c8f4e17f636275ac2ca112f6719cf0ac64cc35
charliekayaker <carlosalfonso.it@gmail.com>
2021-10-31 00:09:27 -0300

    [JIRA-1234] - ShoppingCart Feature - first upload, tests. TDD Approach

---
+++ src/test/java/com/shoppingcart/ShoppingCart/ShoppingCartTests.java
@@ -0,0 +1,34 @@
+package com.shoppingcart.ShoppingCart;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShoppingCartTests{
+      
+    
+      @Test
+      public void getShoppingCart(){
+         
+         ShoppingCart shoppingCart = service.getShoppingCart();
+         Object obj = shoppingCart.getStatus();
+         Assert.assertTrue( obj instanceof Enum);
+         Assert.assertTrue( shoppingCart.getStatus() == StatusCart.PENDING);
+         Assert.assertTrue( shoppingCart.getQuantify() == 0);        
+      }  
+
+      @Test
+      public void getProduct(){
+          var sku = "AR-15657";
+          Product product = service.getProduct(sku);
+          Assert.assertTrue( product.getName().equals("CamisetaArgentina"));
+          Assert.assertTrue( product.getDesription().equals("Camiseta de Argentina 2021 Talle L"));
+          Assert.assertTrue( product.Precio() == 6535);
+      }
+
+      @Test
+      public void getProductWithDiscount(){
+          var sku = "AR-15657";
+          Product product = service.getProductWithDiscount(sku);
+          Assert.assertTrue( product.Precio() == 6535/2);
+      }
+}
